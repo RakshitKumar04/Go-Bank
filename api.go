e26bb84bafc43648506a35d66a6502c817b83cb3
RakshitKumar04 <kumarrakshit0402@gmail.com>
2024-08-14 03:13:11 +0530

    Handled Error

--- api.go
+++ api.go
@@ -1,15 +1,44 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+	"github.com/gorilla/mux"
+)
+
+func WriteJSON(w http ResponseWriter, status int, v any) error {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	return json.NewEncoder(w).Encode(v)
+}
+
+type apiFuncfunc(http.ResponseWriter, *http.Request) error
+
+type ApiError struct {
+	Error string
+}
+
+func makeHTTPHandleFunc(f apiFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if err := f(w, r); err != nil {
+			WriteJSON(w, http.StatusBadRequest, ApiError{Error: err.Error()})
+		}
+	}
+}
 
 type APIServer struct {
 	listenAddr string
 }
 
-func NeApiSerer(listenAddr string) *APIServer {
+func NewApiSerer(listenAddr string) *APIServer {
 	return &APIServer{
 		listenAddr: listenAddr,
 	}
+}
+
+func (s *APIServer) Run() {
+	router := mux.NewRouter()
+	router.HandleFunc("/account", s.handleAccount)
+	
 }
 
 func (s *APIServer) handleAccount(w http.ResponseWriter, r *http.Request) error {
