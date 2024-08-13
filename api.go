886aa902f0ff6dcbb20ebd851f54da2b8e10658d
RakshitKumar04 <kumarrakshit0402@gmail.com>
2024-08-14 03:21:48 +0530

    Setting up APIs

--- api.go
+++ api.go
@@ -7,7 +7,7 @@ import (
 	"github.com/gorilla/mux"
 )
 
-func WriteJSON(w http ResponseWriter, status int, v any) error {
+func WriteJSON(w http.ResponseWriter, status int, v any) error {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(status)
 	return json.NewEncoder(w).Encode(v)
