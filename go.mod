80a076bcc6fe39a8b6e23688faf6d99b17536ebd
RakshitKumar04 <kumarrakshit0402@gmail.com>
2024-08-21 15:45:32 +0530

    Added create account

--- go.mod
+++ go.mod
@@ -4,4 +4,7 @@ go 1.22.2
 
 require github.com/gorilla/mux v1.8.1
 
-require github.com/lib/pq v1.10.9 // indirect
+require (
+	github.com/google/go-querystring v1.1.0 // indirect
+	github.com/lib/pq v1.10.9 // indirect
+)
--- storage.go
+++ storage.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
 	_ "github.com/lib/pq"
 )
 
@@ -50,7 +51,23 @@ func(s *PostgresStore) createAccountTable() error{
 	return err
 }
 
-func(s *PostgresStore) CreateAccount(*Account) error {
+func(s *PostgresStore) CreateAccount(acc *Account) error {
+	query := `INSERT INTO account (first_name, last_name, number, balance, created_at) 
+		VALUES ($1, $2, $3, $4, $5)`
+	
+	resp, err := s.db.Query(
+		query, 
+		acc.FirstName, 
+		acc.LastName, 
+		acc.Number, 
+		acc.Balance, 
+		acc.CreatedAt,
+	)
+
+	if err != nil {
+		return err
+	}
+	fmt.Printf("%+v\n", resp)
 	return nil
 }	
 
